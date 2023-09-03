03b1300ddcf94980bc1a01eb9e4908a44811e3a1
caffre <jaffreagustin18205@gmail.com>
2023-09-02 21:30:16 -0300

    Iteracion1 semicompleta

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -4,23 +4,21 @@ namespace TrabajoSube;
 
 use PHPUnit\Framework\TestCase;
 
-class ColectivoTest extends TestCase{
+class ColectivoTest extends TestCase{  
+  public function testGetlinea(){
+    $cole = new Colectivo(103);
+    $this->assertEquals($cole->getLinea(), 103);
+  }
+  
   public function testPagar(){
-    $saldos = [150, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 2000, 2500, 3000, 3500, 4000];
+    $tarjeta = new Tarjeta(380);
+    $colectivo = new Colectivo();
+    $colectivo->pagarCon($tarjeta);
 
-    $saldosFinales = [30, 80, 130, 180, 230, 280, 330, 380, 480, 580, 680, 780, 880, 980, 1080, 1180, 1280, 1380, 1880, 2380, 2880, 3380, 3880];
-    
-    foreach ($saldos as &$saldo){
-      $tarjeta = new Tarjeta($saldo);
-      $colectivo = new Colectivo();
-      $colectivo->pagarCon($tarjeta);
-      $saldo = $tarjeta->saldo;
-    }
-
-     $this->assertEquals(
-            $saldosFinales,
-            $saldos,
-            "actual value is not equals to expected"
-        );
+    $this->assertEquals(
+      $tarjeta->saldo,
+      260,
+      "actual value is not equals to expected"
+    );
   }
 }
---
+++ tests/TarjetaTest.php
@@ -0,0 +1,52 @@
+<?php 
+
+namespace TrabajoSube;
+
+use PHPUnit\Framework\TestCase;
+
+class TarjetaTest extends Testcase{
+  public function testCargar(){
+    /*$cargas = [150, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 2000, 2500, 3000, 3500, 4000];
+
+    $cargasNovalid = [125, 375, 425, 475, 575, 675, 1375, 1475, 1975, 2475, 2975, 3475, 3975];
+
+    $cargaExcedida = 4000;*/
+
+    $tarjeta1 = new Tarjeta(0);
+    $this->assertTrue($tarjeta1->cargarSaldo(3500));
+    $tarjeta1->cargarSaldo(3500);
+    $this->assertEqual($tarjeta1->saldo, 3500);
+
+    $tarjeta2 = new Tarjeta(5000);
+    $this->assertFalse($tarjeta2->cargarSaldo(2000));
+    $tarjeta2->cargarSaldo(2000);
+    $this->assertEqual($tarjeta2->saldo, 5000);
+
+    $tarjeta3 = new Tarjeta(0);
+    $this->assertFalse($tarjeta3->cargarSaldo(245));
+    $tarjeta3->cargarSaldo(245);
+    $this->assertEqual($tarjeta3->saldo, 0);
+
+    /*
+      $this->assertEquals(
+        $tarjeta->cargarSaldo($carga),
+        0,
+        "Algo salio mal"
+      );
+
+    foreach ($cargasNovalid as &$carga){
+      $this->assertEquals(
+        $tarjeta->cargarSaldo($carga),
+        2,
+        "El mensaje del test es incorrecto"
+      );
+    }
+    
+    $tarjeta = new Tarjeta(4000);
+      $this->assertEquals(
+        $tarjeta->cargarSaldo($cargaExcedida),
+        1
+      );
+    */
+  }
+}
