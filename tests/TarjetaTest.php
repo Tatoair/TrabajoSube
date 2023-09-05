6537d0643597154b94a3c6d5dff51d784627e443
Ayrton Cuffaro <101194995+Tatoair@users.noreply.github.com>
2023-09-04 21:31:51 -0300

    Clases MedioBoleto y FranquiciaCompleta añadidas

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -6,16 +6,44 @@ use PHPUnit\Framework\TestCase;
 
 class TarjetaTest extends Testcase{
   public function testCargar(){
+    //Test de carga simple
     $tarjeta1 = new Tarjeta();
     $this->assertTrue($tarjeta1->cargarSaldo(3500));
     $this->assertEquals($tarjeta1->saldo, 3500);
 
+    //Test de carga excediendo el máximo
     $tarjeta2 = new Tarjeta(5000);
     $this->assertFalse($tarjeta2->cargarSaldo(2000));
     $this->assertEquals($tarjeta2->saldo, 5000);
 
+    //Test de carga con monto no valido
     $tarjeta3 = new Tarjeta();
     $this->assertFalse($tarjeta3->cargarSaldo(245));
     $this->assertEquals($tarjeta3->saldo, 0);
+  }
+
+  public function testDescontar(){
+    //Test de descontar simple
+    $tarjeta = new Tarjeta(200);
+    $tarjeta->descontarSaldo();
+    $this->assertEquals($tarjeta->saldo, 80);
+  }
+}
+
+class MedioBTest extends Testcase{
+  public function testDescontar(){
+    //Test de descontar para medio boleto//
+    $tarjeta = new MedioBoleto(100);
+    $tarjeta->descontarSaldo();
+    $this->assertEquals($tarjeta->saldo, 40);
+  }
+}
+
+class FranqCompTest extends Testcase{
+  public function testDescontar(){
+    //Test de descontar para franquicia completa//
+    $tarjeta = new FranquiciaCompleta(100);
+    $tarjeta->descontarSaldo();
+    $this->assertEquals($tarjeta->saldo, 100);
   }
 }
