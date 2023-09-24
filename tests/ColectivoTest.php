d91de6307e333064ca5be4bdf884601f66cd2353
jaffreagust <jaffreagustin18205@gmail.com>
2023-09-23 21:44:45 -0300

    ColectivoTest.php mejorado

--- tests/ColectivoTest.php
+++ tests/ColectivoTest.php
@@ -13,7 +13,7 @@ class ColectivoTest extends TestCase{
   public function testPagar(){
     $tarjeta = new Tarjeta(380);
     $colectivo = new Colectivo(103);
-    $boleto = $colectivo->pagarCon($tarjeta)
+    $boleto = $colectivo->pagarCon($tarjeta);
     $this->assertEquals($boleto->getSaldo(), 260);
 
     $tarjeta1 = new Tarjeta(-200);
