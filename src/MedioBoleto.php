b934e08ed9e15abb0e2ff1509a6e2bef4ab6ec3b
jaffreagust <jaffreagustin18205@gmail.com>
2023-09-26 16:37:32 -0300

    LimitMedioBoleto iniciado

--- src/MedioBoleto.php
+++ src/MedioBoleto.php
@@ -1,9 +1,45 @@
 <?php
 namespace TrabajoSube;
 class MedioBoleto extends Tarjeta{
+  protected $ultimoViaje;
+  protected $ultimoDia;
+  protected $cantViajes;
+
   public function __construct($saldo = 0){
     $this->ID = uniqid();
     $this->saldo = $saldo;
     $this->tarifa = 60;
+    $this->ultimoViaje = time()-5*60;
+    $this->ultimoDia = strtotime("today");
+    $this->cantViajes = 4;
+  }
+
+  public function setUltimoViaje($tiempo){
+    $this->ultimoViaje = $tiempo;
+  }
+
+  public function setUltimoDia($dia){
+    $this->ultimoDia = $dia;
+  }
+
+  public function descontarSaldo(){
+    //Reinicia la cantidad de viajes medio boleto
+    if (strtotime("today")-$this->ultimoDia > 0){
+      $this->cantViajes = 4;
+      $this->ultimoDia = strtotime("today");
+    }
+
+    if ($this->saldo - $this->tarifa >= -211.84 && (time()-$ultimoViaje)/60 >= 5){
+      if(cantViajes > 0){
+        $this->saldo-=$this->tarifa;
+        $this->cantViajes--;
+      } else {
+        $this->saldo-=$this->tarifa*2;
+      }
+      $this->ultimoViaje = time();
+      return true;
+    } else {
+      return false;
+    }
   }
 }
--- tests/MedioBoletoTest.php
+++ tests/MedioBoletoTest.php
@@ -10,4 +10,36 @@ class MedioBoletoTest extends Testcase{
     $tarjeta->descontarSaldo();
     $this->assertEquals($tarjeta->getSaldo(), 40);
   }
+
+  public function limitMedioBoleto(){
+    //Test para probar que se verifique que pasen 5 minutos antes que se page el siguiente// 
+    $tarjeta = new MedioBoleto(600);
+    $tarjeta->descontarSaldo();
+    $this->assertFalse($tarjeta->descontarSaldo());
+
+
+    //Test para probar que cuenta con 4 medioboletos, y el quinto se cobra normal//
+    $tarjeta1= new MedioBoleto(600);
+    //Viaje 1 , saldo restante: 540
+    $tarjeta1->descontarSaldo();
+    $tarjeta1->setUltimoViaje(time() - 5*60);
+    //Viaje 2, saldo restante: 480
+    $tarjeta1->descontarSaldo();
+    $tarjeta1->setUltimoViaje(time() - 10*60);
+    //Viaje 3, saldo restante: 420
+    $tarjeta1->descontarSaldo();
+    $tarjeta1->setUltimoViaje(time() - 15*60);
+    //Viaje 4, saldo restante:360
+    $tarjeta1->descontarSaldo();
+    $tarjeta1->setUltimoViaje(time() - 20*60);
+    //Viaje 5, NO mas MEDIO BOLETO por el resto del día, saldo restante: 240//
+    $tarjeta1->descontarSaldo();
+    $this->assertTrue($tarjeta1->getSaldo(), 240);
+
+    //Test para probar que al pasar el día vuelven sus 4 medio boletos//
+    $this->setUltimoDia("tomorrow");
+    //Viaje 6, Vuelve el medio boleto, saldo restante: 180//
+    $tarjeta1->descontarSaldo();
+    $this->assertTrue($tarjeta1->getSaldo(), 180);
+  }
 }
