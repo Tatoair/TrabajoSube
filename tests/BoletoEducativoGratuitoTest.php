b7e3a3bafddbe7b68f8c8146fbe183f2efbd883e
Ayrton Cuffaro <101194995+Tatoair@users.noreply.github.com>
2023-10-01 14:05:21 -0300

    Aumento del alcance del test

--- tests/BoletoEducativoGratuitoTest.php
+++ tests/BoletoEducativoGratuitoTest.php
@@ -7,20 +7,22 @@ use PHPUnit\Framework\TestCase;
 class BoletoEducativoGratuitoTest extends Testcase{
   public function testDescontar(){
     //Test para probar que una tarjeta de boleto educativo gratuito no pueda hacer más de 2 gratis por día
-    $tarjeta = new BoletoEducativoGratuito(200);
+    $tarjeta = new BoletoEducativoGratuito(-80);
     //Se descuenta una vez, paga 0
     $tarjeta->descontarSaldo();
-    $this->assertEquals($tarjeta->getSaldo(),200);
+    $this->assertEquals($tarjeta->getSaldo(),-80);
     //Se descuenta otra vez, paga 0
     $tarjeta->descontarSaldo();
-    $this->assertEquals($tarjeta->getSaldo(),200);
+    $this->assertEquals($tarjeta->getSaldo(),-80);
     //Se descuenta una vez más, paga 120
     $tarjeta->descontarSaldo();
-    $this->assertEquals($tarjeta->getSaldo(),80);
+    $this->assertEquals($tarjeta->getSaldo(),-200);
+    //No puede pagar nuevamente al no tener saldo
+    $this->assertFalse($tarjeta->descontarSaldo());
 
     //Probamos que al día siguiente vuelvan los boletos gratuitos
     $tarjeta->setUltimoDia("yesterday");
     $tarjeta->descontarSaldo();
-    $this->assertEquals($tarjeta->getSaldo(),80);
+    $this->assertEquals($tarjeta->getSaldo(),-200);
   }
 }
