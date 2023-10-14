ad2928e4f5d4eb47176cdea82e2ecda33bfc30f7
caffre <jaffreagustin18205@gmail.com>
2023-10-14 10:39:57 -0300

    arreglado construct de Interurbano

--- src/Interurbano.php
+++ src/Interurbano.php
@@ -4,7 +4,7 @@ class Interurbano extends Colectivo{
   protected $linea;
   protected $tarifa;
 
-  public function __construct($linea, $tarifa){
+  public function __construct($linea){
     $this->linea = $linea;
     $this->tarifa = 184;
   }
